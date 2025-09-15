a22c1890ff2d285c28e3b1f1001d3381c3dcac97
Deholder <yesxiaoduan@qq.com>
2025-09-15 10:45:12 +0800

    小命名问题

--- src/Enum/InstanceStateMaskEnum.cpp
+++ src/Enum/InstanceStateMaskEnum.cpp
@@ -8,7 +8,7 @@ DDS::InstanceStateMask getMask(InstanceStateMaskEnum mask)
 // 绑定到 Python
 void init_InstanceStateMaskEnum(py::module_ &m)
 {
-  py::enum_<InstanceStateMaskEnum>(m, "InstanceState", py::arithmetic())
+  py::enum_<InstanceStateMaskEnum>(m, "InstanceStateMask", py::arithmetic())
       .value("ALIVE_INSTANCE_STATE", InstanceStateMaskEnum::ALIVE_INSTANCE_STATE_ENUM)
       .value("NOT_ALIVE_DISPOSED_INSTANCE_STATE", InstanceStateMaskEnum::NOT_ALIVE_DISPOSED_INSTANCE_STATE_ENUM)
       .value("NOT_ALIVE_NO_WRITERS_INSTANCE_STATE", InstanceStateMaskEnum::NOT_ALIVE_NO_WRITERS_INSTANCE_STATE_ENUM)
